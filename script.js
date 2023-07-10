bdc7e79ebe606bc92a79ea52af88d1373f6591e5
seanaelius <dc.seanaelius@gmail.com>
2023-07-10 17:46:13 -0400

    Add classes for each button in index.html Define buttons in script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//ADDS TO EXPRESSION BEING CURRENTLY EVALUATED
+//CLICK EQUALS COMPUTES THE RESULT
+
+//IF EXPRESSION HAS ANY FLAWS (INCORRECT USE OF SYMBOL, DECIMAL, BRACKET)
+//PUT SYNTAX ERROR
+
+//IF DIVIDE BY 0
+//PUT MATH ERROR
+
+//DEFINE ALL BUTTONS
+const clear = document.querySelector('.ac')
+const del = document.querySelector('.del')
+const percentage = document.querySelector('.percent')
+const equal = document.querySelector('.equal')
+const negative = document.querySelector('.negative')
+
+//DEFINE OPERATORS
+const add = document.querySelector('.add')
+const sub = document.querySelector('.sub')
+const divide = document.querySelector('.divide')
+const mul = document.querySelector('.mul')
+
+//DEFINE VALUES
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
+const zero = document.querySelector('.zero')
+
+
+
+
+
+//ADD EVENT LISTENERS TO EACH BUTTON
