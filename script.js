d6da91545d0ceb026e124a0efe1a427d14ddd4db
seanaelius <dc.seanaelius@gmail.com>
2023-07-15 19:10:04 -0400

    Create function that displays numbers and adds them to array

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 //DEFINE DISPLAY AND EVALUATION SCREEN
 const answerDisplay = document.querySelector('.answer')
 const expressionDisplay = document.querySelector('.expression')
+
+//DEFINE CURRENT VALUE, PREVIOUS VALUE, EXPRESSION BEING EVALUATED
+let currValue = ""
+let prevValue = ""
+let expression = []
+
+
+function value(num) {
+    //SHOW IN EXPRESSION DISPLAY
+    expressionDisplay.textContent += `${num}`
+    //ADD TO CURRENT VALUE STRING
+    currValue += `${num}`
+}
